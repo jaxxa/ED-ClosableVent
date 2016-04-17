0dadf4a0d62defa29553ed83deb126a524861b7e
Jaxxa <jaxxa90@gmail.com>
2016-04-17 10:40:28 +1000

    Initial A13 Version.

---
+++ Source/ED-ClosableVent/Building_ClosableVent.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace Enhanced_Development.Temperature
+{
+    public class Building_ClosableVent : RimWorld.Building_Vent
+    {
+        public override void SpawnSetup()
+        {
+            base.SpawnSetup();
+        }
+
+        private CompFlickable FlickComp
+        {
+            get
+            {
+                if (this.m_FlickComp == null)
+                {
+                    this.m_FlickComp = this.GetComp<CompFlickable>();
+                }
+                return this.m_FlickComp;
+            }
+        }
+        private CompFlickable m_FlickComp;
+
+
+        public override void TickRare()
+        {
+            if (this.FlickComp.SwitchIsOn)
+            {
+                base.TickRare();
+            }
+        }
+
+        public override string GetInspectString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+
+            if (this.FlickComp.SwitchIsOn)
+            {
+                stringBuilder.Append(base.GetInspectString());
+            }
+            else
+            {
+                stringBuilder.AppendLine("Vent Closed");
+            }
+
+            return stringBuilder.ToString();
+        }
+
+    }
+
+}
+
